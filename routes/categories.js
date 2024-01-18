17e7fa6a9ca01cc0053fb05c53ee0688e99c6c62
mariam <mariamabdulla475@gmail.com>
2024-01-18 11:57:03 +0300

    categories model crud

---
+++ routes/categories.js
@@ -0,0 +1,26 @@
+// Load express module
+const express = require('express');
+
+// Initialize router functionality from express framework.
+const router = express.Router();
+
+router.use(express.json());
+
+// Require courses controller
+const categoriesCtrl = require('../controller/categories');
+
+
+//Routs
+router.get("/add", categoriesCtrl.categories_add_get);
+router.post("/add",categoriesCtrl.categories_add_post);
+router.get("/index", categoriesCtrl.categories_index_get);
+router.get("/detail/:id", categoriesCtrl.categories_show_get);
+router.delete("/delete", categoriesCtrl.categories_delete_get);
+router.get("/edit", categoriesCtrl.categories_edit_get);
+router.put("/update", categoriesCtrl.categories_update_post);
+
+
+
+
+//Export
+module.exports = router;
