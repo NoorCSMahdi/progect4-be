74b2669d26fde28ccaa9a1243226fe50a1e5f373
salma45 <salmaa-125@outlook.com>
2024-01-18 12:57:58 +0300

    server.js db.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,19 @@
-//Define all APIs or Functions
+// //Define all APIs or Functions
 
-//Export the Modules
-module.exports = {
-    index: function (req, res) {
-        // This will render the index in home folder as the main folder /
-        // res.render("home/index", {title: "Welcome to Wild Wonders App"});
-        console.log("I am here");
-        res.json("OK")
-    },
-};
+// //Export the Modules
+// module.exports = {
+//     index: function (req, res) {
+//         // This will render the index in home folder as the main folder /
+//         // res.render("home/index", {title: "Welcome to Wild Wonders App"});
+//         console.log("I am here");
+//         res.json("OK")
+//     },
+// };
+
+///API's
+
+
+///index 
+exports.index_get = (req,res) => {
+    res.render('home/index',{message: ""})
+}
