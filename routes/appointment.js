9f89f94f03c3544a7b73ef2b668a06a951309eb4
salma45 <salmaa-125@outlook.com>
2024-01-18 10:25:02 +0300

    index, quotation and appointment MVC

---
+++ routes/appointment.js
@@ -0,0 +1,22 @@
+const express= require('express');
+// const methodOverride= require('method-override')
+const router= express.Router();
+router.use(express.urlencoded({extended: true}));
+router.use(express.json());
+const AppointmentCntrl= require("../controllers/appointment");
+// const isLoggedIn = require('../helper/isLoggedIn');
+
+
+//Routes
+router.get("/add", AppointmentCntrl.appointment_create_get);
+router.post("/add", AppointmentCntrl.appointment_create_post);
+router.get("/index", AppointmentCntrl.appointment_index_get);
+router.get("/detail", AppointmentCntrl.appointment_show_get);
+router.get("/delete", AppointmentCntrl.appointment_delete_get);
+router.get("/edit", AppointmentCntrl.appointment_edit_get);
+router.post("/update", AppointmentCntrl.appointment_update_post);
+
+
+module.exports = router;
+
+
