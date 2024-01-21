eadd573f014bce2185e667dfe0849d80f4d560bc
salma45 <salmaa-125@outlook.com>
2024-01-21 14:53:02 +0300

    a

--- routes/quotation.js
+++ routes/quotation.js
@@ -12,7 +12,7 @@ router.get("/add", QuotationCntrl.quotation_create_get);
 router.post("/add", QuotationCntrl.quotation_create_post);
 router.get("/index", QuotationCntrl.quotation_index_get);
 router.get("/detail", QuotationCntrl.quotation_show_get);
-router.delete("/delete", QuotationCntrl.quotation_delete_get);
+router.get("/delete", QuotationCntrl.quotation_delete_get);
 router.get("/edit", QuotationCntrl.quotation_edit_get);
 router.post("/update", QuotationCntrl.quotation_update_post);
 
